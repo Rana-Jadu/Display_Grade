05ea5a67bc8793be95a69a1b28d47e369a86fd4e
Rana <rana.jadu04@gmail.com>
2024-09-11 17:19:42 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,66 @@
+fun main() {
+
+
+//    val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//
+//    val sumOfEvenNumbers = sumEvenNumbers(numbers)
+//    println("Sum of even numbers: $sumOfEvenNumbers")
+//    }
+//
+//    fun sumEvenNumbers(numbers: IntArray): Int {
+//        var sum = 0
+//        for (number in numbers) {
+//            if (number % 2 == 0) {
+//                sum = sum + number
+//            }
+//        }
+//        return sum
+
+
+//    first@ for (i in 1..10) {
+//        swcond@ for (j in 1..5) {
+//            println("i = $i   j = $j")
+//            if (i ==2)
+//                break@swcond
+//        }
+//    }
+
+
+//    val x = arrayOf(1, 2, 3, 4, 5, 6, 7)
+//    for (item in x) println(item)
+
+//    var sum = 0
+//    for (i in x)
+//    {
+//        sum += i
+//        if (sum > 20)
+//            break
+//    }
+//    println(sum)
+
+
+//    var i = 1
+//    while (i < 5){
+//        println(i)
+//        i++
+//    }
+
+//    do {
+//        println(i)
+//        i++
+//    }while (i < 5)
+
+//    for (i in 1..10)
+//        println(i)
+
+//   var grade = 100
+//        when (grade) {
+//            in 90..100 -> println("A")
+//            in 80..89 ->  println("B")
+//            in 70..79 ->  println("C")
+//            in 60..69 -> println("D")
+//            else -> "F"
+//        }
+}
+
+
